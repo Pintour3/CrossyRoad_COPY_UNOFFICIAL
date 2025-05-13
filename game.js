bd34b44a67f43fb3db4b324be1f36f4de4fad3d5
Arthur Maye <arthurmaye2017@gmail.com>
2025-05-13 13:44:10 +0200

    preparing procedural logic

--- game.js
+++ game.js
@@ -3,6 +3,7 @@ import {GLTFLoader} from "three/addons/loaders/GLTFLoader.js";
 import { CSS2DRenderer,CSS2DObject } from 'three/addons/renderers/CSS2DRenderer.js';
 import Stats from 'three/addons/libs/stats.module.js';
 const scene = new THREE.Scene();
+//camera
 const camera = new THREE.PerspectiveCamera(65,window.innerWidth/window.innerHeight,0.1,1000)
 camera.position.set(4.2,5,4.3)
 
@@ -24,6 +25,9 @@ document.body.appendChild( stats.dom );
 
 let objects = new Map()
 //init
+const grassGeo = new THREE.BoxGeometry(1,0.125,1)
+const dirtGeo = new THREE.BoxGeometry(1,0.125,1)
+const terrainTypes = ["grass","river","road","train"]
 let map = [
         [1,1,1,1,1,1,1,1,1],
         [1,1,1,1,1,1,1,1,1],
@@ -65,8 +69,10 @@ function init(){
     directionalLight.shadow.camera.near = 0.1;
     directionalLight.shadow.camera.far = 100;
     scene.add(directionalLight)
-    const helper = new THREE.CameraHelper(directionalLight.shadow.camera);
+    //enable if needed
+    /*const helper = new THREE.CameraHelper(directionalLight.shadow.camera);
     scene.add(helper)
+    */
     //draw Trees with variable size with coords parameters
     function drawTree(x,y,z){
         const key = `${x},${y},${z}`
@@ -85,37 +91,23 @@ function init(){
                     child.material.side = THREE.FrontSide
                 }
             }) 
+            //if there isn't objects at this position
             if (!objects.has(key)) {
+                //create an id in the map
                 objects.set(key, [])
             }
-        
+            //and add the tree to this map id (same for all the others)
             objects.get(key).push(tree)
             scene.add(tree)
         })
     }
-    //draw blocs on the sides of the game grid
-    function drawSide(){
-        map.forEach((lines,index)=>{
-            let type = lines[0]
-            switch(type){
-                case 1:
-                    for (let k = 0; k < 5;k++) {
-                        grassBlock(index,0, -(k + 1),0x8EC045)
-                        grassBlock(index,0,lines.length + k,0x8EC045)
-                        drawTree(index,0,lines.length + k)
-                        drawTree(index,0,-(k + 1))
-                    }
-                    break
-            }
-        })
-    }
+    
     
 
     //grassblock func
     function grassBlock(x,y,z,color) {
         const key = `${x},${y},${z}`
-        const grassGeo = new THREE.BoxGeometry(1,0.25,1)
-        const dirtGeo = new THREE.BoxGeometry(1,0.25,1)
+        
         
         const grass = new THREE.MeshStandardMaterial({color:color});
         const dirt = new THREE.MeshBasicMaterial({color:0x403410});
@@ -125,7 +117,7 @@ function init(){
         grassBlock.receiveShadow = true
         dirtBlock.receiveShadow = false
         grassBlock.position.set(x,y,z)
-        dirtBlock.position.set(x,y-0.25,z)
+        dirtBlock.position.set(x,y-0.125,z)
         
         if (!objects.has(key)) {
             objects.set(key, [])
@@ -135,6 +127,7 @@ function init(){
         scene.add(grassBlock)
         scene.add(dirtBlock)
     }
+    //initialise the map, drawn using the map list configuration
     function drawMap(){
         map.forEach((row,rowIndex)=>{
             row.forEach((col,colIndex)=>{
@@ -142,6 +135,22 @@ function init(){
                     grassBlock(rowIndex,0,colIndex,0xBDF566)
                 }
             })
+        })
+    }
+    //initialise the side map, drawn usning the map list configuration
+    function drawSide(){
+        map.forEach((lines,index)=>{
+            let type = lines[0]
+            switch(type){
+                case 1:
+                    for (let k = 0; k < 19;k++) {
+                        grassBlock(index,0, -(k + 1),0x8EC045)
+                        grassBlock(index,0,lines.length + k,0x8EC045)
+                        drawTree(index,0,lines.length + k)
+                        drawTree(index,0,-(k + 1))
+                    }
+                    break
+            }
         })
     }
     //completely delete a block from the scene
@@ -152,6 +161,7 @@ function init(){
                 for (const mesh of meshes) {
                     scene.remove(mesh)
                     mesh.geometry?.dispose()
+                    mesh.parent?.remove(mesh);
                     if (Array.isArray(mesh.material)){
                         for (const mat of mesh.material)
                             mat.dispose()
@@ -162,23 +172,64 @@ function init(){
                 objects.delete(key)   
             }
     }
+    
     //build new terrain as the chicken is walking
     function updateMap(){
-        //if chicken is reaching the farest point
+        //if chicken is reaching the furthest point
         if (chicken.position.x > playerLastPosition) {
             playerLastPosition = chicken.position.x
             //deleting the last line of blocs
             shiftMap()
             //adding a line to the top of the path
-            let list = [1,1,1,1,1,1,1,1,1]
+            //rules for the procedural generation
+            //max 5 identic path in a row and minimum 2 in a row
+            //initial probability : 40% grass, 30% road, 20% river, 10% train --> update if needed
+            //grass = 1, road = 2, river = 3, train = 4
+            let material;
+            let lastPath = []
+            for (let k = 5; k > 0;k --) {
+                //list of the previous path
+                lastPath.push((map[(map.length - 1)-k][0]))
+            }
+            function isTheSame(elt) {
+                return elt == lastPath[0]
+            }
+                
+            if (lastPath.every(isTheSame)){
+                console.log("same")
+            }
+            var random = Math.random().toFixed(2)
+            if (random <= 0.4) {
+                material = 1
+            } else if (random > 0.4 && random <= 0.7) {
+                material = 2
+            } else if (random > 0.7 && random <= 0.9) {
+                material = 3 
+            } else {
+                material = 4
+            }
+            let middleColor, sideColor;
+            switch(material){
+                case 1:
+                    //grass
+                    middleColor = 0xBDF566
+                    sideColor = 0x8EC045
+                case 2:
+                    //road 
+                    middleColor = 0x535864
+                    sideColor = 0x49505B
+                    //here lets try to import a road texture 
+            }
+            
+            const list = [1,1,1,1,1,1,1,1,1]
             map.push(list)
             list.forEach((elt,index)=>{
-                grassBlock(map.length-1,0,index,0xBDF566)
+                grassBlock(map.length-1,0,index,middleColor)
             })
             //now generate the sides of the map
             for (let k = 0;k<5;k++){
-                grassBlock(map.length-1,0,-(1 + k),0x8EC045)
-                grassBlock(map.length-1,0,list.length + k,0x8EC045)
+                grassBlock(map.length-1,0,-(1 + k),sideColor)
+                grassBlock(map.length-1,0,list.length + k,sideColor)
                 drawTree(map.length-1,0,-(1 + k))
                 drawTree(map.length-1,0,list.length + k)
             }
@@ -237,9 +288,10 @@ function init(){
         //putting the head in the right way
         chicken.rotation.y = Math.PI * 0.5
         //scene and debug
-        
-        chickenBoxHelper = new THREE.BoxHelper(chicken,0x00ff00);
+        //add if needed
+        /*chickenBoxHelper = new THREE.BoxHelper(chicken,0x00ff00);
         scene.add(chickenBoxHelper)
+        */
         camera.lookAt(chicken.position)
         scene.add(chicken)
     })
@@ -303,21 +355,25 @@ function init(){
                 break
             case "d":
             case "ArrowRight":
-                currentRotation = shortestRotation(currentRotation,0)
-                tl.to(chicken.position,{y:chicken.position.y + 0.5,duration:0.1,ease:"power1.out",})
-                .to(chicken.rotation,{y: currentRotation,duration: 0.2,ease:"power1.inOut"},"<")
-                .to(chicken.position,{z:chicken.position.z + 1,duration:0.1,ease:"power1.inOut",},"<")
-                .to(chicken.position,{y:chicken.position.y,duration:0.1,ease:"power1.in",},"-=0.15")
-                gsap.to(camera.position,{z:camera.position.z + 1,duration:1,ease:"power1.out"})
+                if (chicken.position.z < 8) {
+                    currentRotation = shortestRotation(currentRotation,0)
+                    tl.to(chicken.position,{y:chicken.position.y + 0.5,duration:0.1,ease:"power1.out",})
+                    .to(chicken.rotation,{y: currentRotation,duration: 0.2,ease:"power1.inOut"},"<")
+                    .to(chicken.position,{z:chicken.position.z + 1,duration:0.1,ease:"power1.inOut",},"<")
+                    .to(chicken.position,{y:chicken.position.y,duration:0.1,ease:"power1.in",},"-=0.15")
+                    gsap.to(camera.position,{z:camera.position.z + 1,duration:1,ease:"power1.out"})
+                }
                 break
             case "a":
-            case "ArrowLeft":   
-                currentRotation = shortestRotation(currentRotation,Math.PI )
-                tl.to(chicken.position,{y:chicken.position.y + 0.5,duration:0.1,ease:"power1.out",})
-                .to(chicken.rotation,{y: currentRotation,duration: 0.2,ease:"power1.inOut"},"<")
-                .to(chicken.position,{z:chicken.position.z - 1,duration:0.1,ease:"power1.inOut",},"<")
-                .to(chicken.position,{y:chicken.position.y,duration:0.1,ease:"power1.in",},"-=0.15")
-                gsap.to(camera.position,{z:camera.position.z - 1,duration:1,ease:"power1.out"})
+            case "ArrowLeft":  
+                if (chicken.position.z > 0) { 
+                    currentRotation = shortestRotation(currentRotation,Math.PI )
+                    tl.to(chicken.position,{y:chicken.position.y + 0.5,duration:0.1,ease:"power1.out",})
+                    .to(chicken.rotation,{y: currentRotation,duration: 0.2,ease:"power1.inOut"},"<")
+                    .to(chicken.position,{z:chicken.position.z - 1,duration:0.1,ease:"power1.inOut",},"<")
+                    .to(chicken.position,{y:chicken.position.y,duration:0.1,ease:"power1.in",},"-=0.15")
+                    gsap.to(camera.position,{z:camera.position.z - 1,duration:1,ease:"power1.out"})
+                }
                 break
         }
     })
@@ -331,9 +387,12 @@ function init(){
 init()
 function animate(){
     stats.begin()
+    //enable if needed
+    /*
     if (chickenBoxHelper) {
         chickenBoxHelper.update()
     }
+    */
     renderer.render(scene,camera)
     stats.end()
 }
